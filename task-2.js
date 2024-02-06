08f8746de7584cbd6ff741854714747bd33cfa1a
Abrarulctg <jubairctg99@gmail.com>
2024-02-07 01:30:21 +0600

    finished task 2

--- task-2.js
+++ task-2.js
@@ -1,4 +1,16 @@
 // Find the friend with the smallest name.
 
+function getSmallestName(strings) {
+    let lowestName = strings[0];
+    for (const str of strings) {
+        if (str.length < lowestName.length) {
+            lowestName = str;
+        }
+    }
+    return lowestName;
+}
 
-const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+const heights2 = ['rahim', 'ab', 'robin', 'rafi', 'ron', 'rashed'];
+
+const res = getSmallestName(heights2);
+console.log(res);
